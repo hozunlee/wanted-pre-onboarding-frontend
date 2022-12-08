d31d4caa076f764d2483cf0749a8ebb7bcec52e2
hozunlee <ho2yahh@gmail.com>
2022-12-08 19:32:59 +0900

    eslint 설정 수정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   env: {
     browser: true,
@@ -6,34 +7,26 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    // 'plugin:@typescript-eslint/recommended',
     'plugin:react/jsx-runtime',
     'plugin:jsx-a11y/recommended',
     'plugin:prettier/recommended',
   ],
   overrides: [],
-  // parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: { jsx: true },
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    // '@typescript-eslint',
-    'react-hooks',
-    'jsx-a11y',
-    'prettier',
-  ],
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'prettier'],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/jsx-uses-react': 'off',
     'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
     'react/prop-types': 'off',
     'prettier/prettier': 'error',
+    'no-unused-vars': 'off',
     quotes: ['error', 'single', { allowTemplateLiterals: true }],
-    'react/display-name': 'off',
   },
   settings: {
     react: { version: 'detect' },
