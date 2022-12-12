de2a511227c4004148a61e35e39f6de1214ab513
hozunlee <ho2yahh@gmail.com>
2022-12-12 11:52:00 +0900

    feat : GA 연결

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
 
+// GA URL 변경을 감지하는 컴포넌트
+import RouteChangeTracker from './utils/RouteChangeTracker';
 // pages
 import Main from './page/Main';
 import Todo from './page/Todo';
@@ -8,6 +10,7 @@ import Todo from './page/Todo';
 import { UserInfoProvider } from './hooks/userInfo-context';
 
 function App() {
+  RouteChangeTracker();
   return (
     <>
       <UserInfoProvider>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+//GA
+import ReactGA from 'react-ga';
+const TRACKING_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID; // 발급받은 추적ID를 환경 변수로 불러온다.
+ReactGA.initialize(TRACKING_ID);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
